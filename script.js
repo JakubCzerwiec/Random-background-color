72735ee3b7efbd9058b1846e475734e11e31bda8
JakubCzerwiec <64986417+JakubCzerwiec@users.noreply.github.com>
2020-05-07 22:00:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const colorBtn = document.querySelector('.colorBtn');
+const colorRdn = document.querySelector('.colorRdn');
+const bodyBcg = document.querySelector('body');
+
+const colors = ['yellow', 'res', 'green', '#3b5998'];
+
+colorBtn.addEventListener('click', changeColor);
+
+function changeColor () {
+    let random = Math.floor(Math.random() * colors.length);
+    bodyBcg.style.backgroundColor = colors[random];
+}
+
+
+colorRdn.addEventListener('click', drawColor);
+
+function drawColor () {
+    let draw1 = Math.floor(Math.random() * 16);
+    let draw2 = Math.floor(Math.random() * 16);
+    let draw3 = Math.floor(Math.random() * 16);
+
+    let arr = ['#', draw1, draw2, draw3, 1];
+    let rndColors = arr.join('');
+    
+    bodyBcg.style.backgroundColor = rndColors;
+}
+    
