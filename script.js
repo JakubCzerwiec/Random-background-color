841e363d2536cd00f1543e4a862e23f5887c85e2
Jakub Czerwiec <64986417+JakubCzerwiec@users.noreply.github.com>
2020-05-14 20:00:25 +0200

    Added some new functions

--- script.js
+++ script.js
@@ -1,51 +1,84 @@
 const colorBtn = document.querySelector('.colorBtn');
 const colorRdn = document.querySelector('.colorRdn');
 const bodyBcg = document.querySelector('body');
+const switchBox = document.querySelector('.switch-box');
+const switchBtn = document.querySelector('.switch');
+const colorName = document.querySelector('.colorName');
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
-const colors = ['yellow', 'res', 'green', '#3b5998'];
+
+const colors = ['yellow', 'red', 'green', '#3b5998'];
+
+switchBtn.addEventListener('click', () => {
+    if (switchBtn.getAttribute('class') == 'switch rgb') {
+        switchBtn.setAttribute('class', 'switch hex')
+    } else {
+        switchBtn.setAttribute('class', 'switch rgb')
+    }
+})
+
 
 colorBtn.addEventListener('click', changeColor);
 
 function changeColor () {
     let random = Math.floor(Math.random() * colors.length);
     bodyBcg.style.backgroundColor = colors[random];
+    
+    colorName.textContent = colors[random];
 }
 
 
 colorRdn.addEventListener('click', drawColor);
+    
+
 
     function drawColor () {
+
+        if (switchBtn.classList.contains('rgb')) {
             let arr = ['rgb(',1,')'];
             let number = [];
-        for (let i = 0; i < 3; i++) {
             
-            let draw = Math.floor(Math.random() * 160);
+            for (let i = 0; i < 3; i++) {
+                
+                let draw = Math.floor(Math.random() * 160);
+                
+                number.push(draw);
             
-            number.push(draw);
-        
-            arr.splice(1,1,number);
+                arr.splice(1,1,number);
 
+            }
+            let arr1 = arr.join('');
+            console.log(arr);
+            bodyBcg.style.backgroundColor = arr1;
+            
+            arr = ['rgb(',1,')'];
+            
+            number = [];
+
+
+            colorName.textContent = arr1;
+
+        } else if (switchBtn.classList.contains('hex')) {
+            let hexColor = '#';
+            for (let i = 0; i < 6; i++) {
+                let randomNumber = Math.floor(Math.random() * hex.length)
+                hexColor += hex[randomNumber]
+            }
+            colorName.textContent = hexColor;
+            bodyBcg.style.backgroundColor = hexColor;
         }
-        let arr1 = arr.join('');
-        console.log(arr);
-        bodyBcg.style.backgroundColor = arr1;
-        
-        arr = ['rgb(',1,')'];
-        number = [];
-}
+        }
 
 
-/*  First attempt, not perfect
-function drawColor () {
-    let draw1 = Math.floor(Math.random() * 160);
-    let draw2 = Math.floor(Math.random() * 160);
-    let draw3 = Math.floor(Math.random() * 160);
 
-    let arr = ['#', draw1, draw2, draw3, 1];
-    let rndColors = arr.join('');
 
-    bodyBcg.style.backgroundColor = rndColors;
-}
-    
-*/
 
+/* switchBtn.addEventListener('click', () => {
+    if (switchBtn.classList.contains('rgb')) {
+        switchBtn.classList.remove('rgb');
+        switchBtn.classList.add('hex');
+    } else if (switchBtn.classList.contains('hex')) {
+        switchBtn.classList.remove('hex');
+        switchBtn.classList.add('rgb');
+    }
+}) */
